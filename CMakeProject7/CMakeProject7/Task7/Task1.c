e2b045c0834f6018ff50976059538f5b71fedbd5
SangrealSoave <48495284+SangrealSoave@users.noreply.github.com>
2019-05-08 17:33:40 +0300

    Update Task1.c

--- CMakeProject7/CMakeProject7/Task7/Task1.c
+++ CMakeProject7/CMakeProject7/Task7/Task1.c
@@ -1,7 +1,7 @@
 ﻿#include < stdio.h >  
 #include < string.h > 
 #include < math.h > 
-enum Zachet { Zachet=5,Nezachet=2 };
+enum Zachet { Zachet = 5, Nezachet = 2 };
 enum Ekzamen { Neud = 2, Udv = 3, Hor = 4, Otl = 5 };
 struct Person {
 	char firstName[20];
@@ -28,6 +28,9 @@ void main() {
 	strcpy_s(p.patronymic, 20, patronimic);
 	strcpy_s(p.number, 10, number);
 	p.sem = 4;
+	char* *zch = "";
+	int i = 0;
+	printf("%s\n", zch);
 m:
 	printf("Number zachetki:%s\n", p.number);
 	printf("Name:     %s\n", p.firstName);
@@ -35,30 +38,43 @@ m:
 	printf("Otchestvo:%s\n", p.patronymic);
 	printf("Semestr:  %i\n", p.sem);
 	printf("\nChose semestr:\n1.Semestr\n2.Semestr\n3.Semestr\n4.Close");
+
 	switch (_getch()) {
 		{case '1':
 			printf("\nChose zachet or ekzamen:\n1.Zachet\n2.Ekzamen\n\n");
 			switch (_getch())
 			{
 			case'1':
+				zch[0] = "2017-2018 god:";
+				zch[1] = " VVP         Maksimov     22.12.17";
+				zch[2] = "SEPIT        Korbleva     25.12.17";
+				zch[3] = "English      Mashenskaya  25.12.17";
+				zch[4] = "RYiKR        Beznosova    25.12.17";
+				zch[5] = "Informatika  Zagvozdkina  26.12.17";
 				printf("Zachety 1 semestr:\n\n");
-				printf("2017/2018 god;\n");
+				printf("%s \n", zch[0]);
 				printf("Predmet     Prepodavatel   Data     Ocenka\n");
-				printf(" VVP         Maksimov     22.12.17   %d\n", z);
-				printf("SEPIT        Korbleva     25.12.17   %d\n", z);
-				printf("English      Mashenskaya  25.12.17   %d\n", z);
-				printf("RYiKR        Beznosova    25.12.17   %d\n", z);
-				printf("Informatika  Zagvozdkina  26.12.17   %d\n", z);
+				for (i = 1; i < 6; i++) {
+					printf("%s  %d\n", zch[i], z);
+				}
+				zch = 0;
+				free(zch);
 				system("pause");
 				system("cls");
 				goto m;
 			case'2':
+				zch[0] = "2017-2018 god:";
+				zch[1] = "Phylosophy  Korableva    26.12.17";
+				zch[2] = "AGiLA       Soliev       26.12.17";
+				zch[3] = "High Math   Raycin       26.12.17 ";
 				printf("Ekzameny 1 semestr:\n\n");
-				printf("2017/2018 god;\n");
+				printf("%s \n", zch[0]);
 				printf("Predmet    Prepodavatel     Data     Ocenka\n");
-				printf("Phylosophy  Korableva    26.12.17    %d\n", o);
-				printf("AGiLA       Soliev       26.12.17    %d\n", u);
-				printf("High Math   Raycin       26.12.17    %d\n", u);
+				printf("%s    %d\n", zch[1], o);
+				printf("%s    %d\n", zch[2], u);
+				printf("%s    %d\n", zch[3], u);
+				zch = 0;
+				free(zch);
 				system("pause");
 				system("cls");
 				goto m;
@@ -75,25 +91,38 @@ m:
 			switch (_getch())
 			{
 			case'1':
+				zch[0] = "2017-2018 god:";
+				zch[1] = "NPBDVI       Antipov      30.05.18";
+				zch[2] = "TOE          Hatuceva     30.05.18";
+				zch[3] = "KG           Ignatov      30.05.18";
+				zch[4] = "Fiz-ra       Simkacheva   30.05.18";
+				zch[5] = "Fizika       Zhilinsky     5.06.17";
 				printf("Zachety 2 semestr:\n\n");
-				printf("2017/2018 god;\n");
+				printf("%s \n", zch[0]);
 				printf("Predmet     Prepodavatel   Data     Ocenka\n");
-				printf("NPBDVI       Antipov      30.05.18   %d\n", z);
-				printf("TOE          Hatuceva     30.05.18   %d\n", z);
-				printf("KG           Ignatov      30.05.18   %d\n", z);
-				printf("Fiz-ra       Simkacheva   30.05.18   %d\n", z);
-				printf("Fizika       Zhilinsky     5.06.17   %d\n", z);
+				for (i = 1; i < 6; i++) {
+					printf("%s  %d\n", zch[i], z);
+				}
+				zch = 0;
+				free(zch);
 				system("pause");
 				system("cls");
 				goto m;
 			case'2':
+				zch[0] = "2017-2018 god:";
+				zch[1] = "Informatika        Zagvozdkina   15.06.18";
+				zch[2] = "History            Kuznetscova   19.06.18";
+				zch[3] = "English            Egorova       25.06.18";
+				zch[4] = "High Math          Raycin        29.06.18";
 				printf("Ekzameny 2 semestr:\n\n");
-				printf("2017/2018 god;\n");
+				printf("%s \n", zch[0]);
 				printf("Predmet           Prepodavatel     Data     Ocenka\n");
-				printf("Informatika        Zagvozdkina   15.06.18     %d\n", h);
-				printf("History            Kuznetscova   19.06.18     %d\n", o);
-				printf("English            Egorova       25.06.18     %d\n", h);
-				printf("High Math          Raycin        29.06.18     %d\n", o);
+				printf("%s     %d\n", zch[1], h);
+				printf("%s     %d\n", zch[2], o);
+				printf("%s     %d\n", zch[3], h);
+				printf("%s     %d\n", zch[4], o);
+				zch = 0;
+				free(zch);
 				system("pause");
 				system("cls");
 				goto m;
@@ -108,34 +137,46 @@ m:
 			switch (_getch())
 			{
 			case'1':
+				zch[0] = "2018-2019 god:";
+				zch[1] = "TFKP         Kuprin      24.12.18";
+				zch[2] = "FOE          Vlasov      25.12.18";
+				zch[3] = "IRSS         Vanushina   26.12.18";
+				zch[4] = "VMPPP        Semenova    27.12.18";
+				zch[5] = "TEClab       Frisk       28.12.18";
+				zch[6] = "TECcr        Frisk       28.12.18";
 				printf("Zachety 3 semestr:\n\n");
-				printf("2018/2019 god;\n");
+				printf("%s \n", zch[0]);
 				printf("Predmet     Prepodavatel   Data     Ocenka\n");
-				printf("TFKP         Kuprin      24.12.18   %d\n", z);
-				printf("FOE          Vlasov      25.12.18   %d\n", z);
-				printf("IRSS         Vanushina   26.12.18   %d\n", z);
-				printf("VMPPP        Semenova    27.12.18   %d\n", z);
-				printf("TEClab       Frisk       28.12.18   %d\n", z);
-				printf("TECcr        Frisk       28.12.18   %d\n", z);
+				for (i = 1; i < 7; i++) {
+					printf("%s  %d\n", zch[i], z);
+				}
+				zch = 0;
+				free(zch);
 				system("pause");
 				system("cls");
 				goto m;
 			case'2':
+				zch[0] = "2018-2019 god:";
+				zch[1] = "Fizika             Zhilinsky     16.01.19";
+				zch[2] = "TViMS              Demin         21.01.19";
+				zch[3] = "VM                 Semenova      25.01.19";
+				zch[4] = "Diskr.Math         Dubnov        31.01.19";
 				printf("Ekzameny 3 semestr:\n\n");
 				printf("2018/2019 god;\n");
 				printf("Predmet           Prepodavatel     Data     Ocenka\n");
-				printf("Fizika             Zhilinsky     16.01.19      %d\n", h);
-				printf("TViMS              Demin         21.01.19      %d\n", h);
-				printf("VM                 Semenova      25.01.19      %d\n", o);
-				printf("Diskr.Math         Dubnov        31.01.19      %d\n", o);
+				printf("%s      %d\n", zch[1], h);
+				printf("%s      %d\n", zch[2], h);
+				printf("%s      %d\n", zch[3], o);
+				printf("%s      %d\n", zch[4], o);
+				zch = 0;
+				free(zch);
 				system("pause");
 				system("cls");
 				goto m;
 			default:
 				break;
-			}
-		}
+			}break;
+		}break;
 	}
-
-
+}
 }
